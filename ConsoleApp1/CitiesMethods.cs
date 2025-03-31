6e5824327809790df3b709dd36655c7ebd662e5f
hernadi.balazs <hernadi.balazs@BURG-1E03-10>
2025-03-31 08:57:49 +0200

    Add project files.

---
+++ ConsoleApp1/CitiesMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class CitiesMethods
+    {
+        public List<City> Cityes = [];
+
+        void LoadFromCSV(string path) => Cityes = File.ReadAllLines(path).Skip(1).Where(x => x.Contains("NaN")).Select(x => new City(x.Split(';'))).ToList();
+
+        /// <summary>
+        /// Elmenti a városok adatait CSV fájlba
+        /// </summary>
+        /// <param name="path">A megadott útvonalon lévő fájlba írja ki az adatokat</param>
+        /// <param name="cities">A városok listája</param>
+        /// <param name="charCode">A karakterkódolást határozzsa meg</param>
+        void SaveToCSV(string path, List<City> cities, string charCode = "UTF-8")
+        {
+
+        }
+
+        int CityCount() => Cityes.Count;
+
+        /// <summary>
+        /// Folyamatosan növekvő-e a megadott népességi adatok sorozata?
+        /// </summary>
+        /// <returns>true esetén igen</returns>
+        bool IsContinuousGrowing(List<int> populationDatas)
+        {
+
+        }
+
+        /// <summary>
+        /// Visszaadja a 10 legnagyobb népességű várost a megadott évben
+        /// </summary>
+        List<City> Top10City(int year)
+        {
+            Cityes.OrderBy(x => x.);
+        }
+    }
+}
---
+++ ConsoleApp1/City.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class City
+    {
+        string city_code, city_name;
+        int y2010, y2020, y2030, y2040, y2050;
+
+        public City(string[] cityes)
+        {
+            this.city_code = cityes[0];
+            this.city_name = cityes[1];
+            this.y2010 = int.Parse(cityes[2]);
+            this.y2020 = int.Parse(cityes[3]);
+            this.y2030 = int.Parse(cityes[4]);
+            this.y2040 = int.Parse(cityes[5]);
+            this.y2050 = int.Parse(cityes[6]);
+        }
+
+        public string City_code { get => city_code; }
+        public string City_name { get => city_name; }
+        public int Y2010 { get => y2010; }
+        public int Y2020 { get => y2020; }
+        public int Y2030 { get => y2030; }
+        public int Y2040 { get => y2040; }
+        public int Y2050 { get => y2050; }
+    }
+}
